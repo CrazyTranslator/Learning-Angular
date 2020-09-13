3e76e1f3aaf6866be00764ad2ae14ff601613aff
CrazyTranslator <bia.curupana@gmail.com>
2020-09-13 18:06:58 -0300

    aula 40
    falta tarefa 3

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,9 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created'
+  serverName = 'TestServer'
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
 
   constructor() {
       setTimeout(() => {
@@ -25,11 +28,14 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'server was created.'
+    this.serverCreated = true; 
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'server was created! Name is' + this.serverName;
+    
   }
 
-  onUpdateServerStatus(event: any){
-    console.log(event);
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
 
   }
 }
